2b6bd16bce38dbbf83ada7c347fbaf715dd0c1b9
zsheperdigian <zsheperdigian@gmail.com>
2024-04-15 20:48:34 -0700

    set up a connection. need to build in tables

--- app.py
+++ app.py
@@ -2,26 +2,26 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
-from sqlite3 import Error
+
 
 # Configure application
 app = Flask(__name__)
 
-def create_connection(path):
-    connection = None
-    try:
-        connection = sqlite3.connect("mealFinder.db")
-        print("Connection to SQLite DB successful")
-    except Error as e:
-        print(f"the error '{e}' occured")
-    
-    return connection
+#setting up sqlite database
+try:
+    connection = sqlite3.connect('foods.db', check_same_thread= False)
+    cursor = connection.cursor()
+    print("DB init")
 
+#handling errors
+except sqlite3.Error as error:
+    print('Error Occured - ', error)
 
 #home route
 @app.route("/")
 def index():
     #home route
+
 
     return render_template("index.html")
 
@@ -30,9 +30,15 @@ def food():
     if request.method == "GET":
         
         #grab the users meal items from DB
+        foods = connection.cursor()
+
+        foods.execute("SELECT * FROM recipes")
+
+        for food in foods:
+            print(food)
 
         #make variables to hold food info in order to be displayed on page
-        return render_template("/food.html")
+        return render_template("/food.html", foods)
     else:
         new_food_item = request.form.get("foodname")
         is_vegan = request.form.get("vegan")
@@ -45,6 +51,10 @@ def food():
         print("is it vegetarian?", is_vegetarian)
         print("does it have dairy?", dairy)
         print("is it gluton free? ", glutonfree)
+
+        connection.execute("INSERT INTO recipes ?", new_food_item)
+
+     
 
         return render_template("/food.html")
 
@@ -65,4 +75,8 @@ def jedi():
 # add food
 
 
-#
+#closing database connection: IS THIS PROPER?? to put it down here?
+""" if connection:
+        connection.close()
+        print("SQL connection closed")
+"""
